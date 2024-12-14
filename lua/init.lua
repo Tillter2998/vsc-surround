375ff10f475765cfa7aa540262047be933408a2c
Tillter2998 <elrich.dev2998@gmail.com>
2024-12-14 12:17:56 -0400

    Initial testing commit

---
+++ lua/init.lua
@@ -0,0 +1,24 @@
+-- local api = vim.api
+
+local M = {}
+
+M.config = {
+
+}
+
+function M.wrap_selected()
+  print("HELLO");
+  vim.cmd('normal! gv"xy')
+  local txt = vim.fn.getreg('x')
+  local tbl = {}
+  for line in txt:gmatch("[^\n]+") do
+    table.insert(tbl, line)
+  end
+  print(tbl)
+end
+
+function M.setup()
+  vim.api.nvim_command("command! -range WrapSelected lua require('vsc_surround').wrap_selected()")
+end
+
+return M
